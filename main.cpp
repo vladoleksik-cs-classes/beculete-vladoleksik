3d144f30c4360a9de584082d3220c60aa29a92b3
Vlad-Andrei Oleksik <107761325+vladoleksik@users.noreply.github.com>
2025-12-02 21:06:32 +0200

    Attempt 1

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <fstream>
+#define MODX 1000000007
 
 using namespace std;
 
@@ -7,6 +8,41 @@ ofstream fout("output.txt");
 
 int main()
 {
-  cout << "Hello World!" << endl;
-  return 0;
+    int n;
+    long long int b00, b01, b10, b11;
+    unsigned int i;
+    long long int b[2][2] = { {1,0},{0,1} }, a[2][2] = { {1,1},{1,0} };
+    fin >> n;
+    n+=2;
+    if (n == 0)
+    {
+        fout << 0;
+        return 0;
+    }
+    for (i = (1 << 31); !(n & i); i >>= 1);
+    for (; i; i >>= 1)
+    {
+        b00 = b[0][0] * b[0][0] + b[0][1] * b[1][0];
+        b01 = b[0][0] * b[0][1] + b[0][1] * b[1][1];
+        b10 = b[1][0] * b[0][0] + b[1][1] * b[1][0];
+        b11 = b[1][0] * b[0][1] + b[1][1] * b[1][1];
+        b00 %= MODX, b01 %= MODX, b10 %= MODX, b11 %= MODX;
+        b[0][0] = b00;
+        b[0][1] = b01;
+        b[1][0] = b10;
+        b[1][1] = b11;
+        if (n & i)
+        {
+            b00 = b[0][0] * a[0][0] + b[0][1] * a[1][0];
+            b01 = b[0][0] * a[0][1] + b[0][1] * a[1][1];
+            b10 = b[1][0] * a[0][0] + b[1][1] * a[1][0];
+            b11 = b[1][0] * a[0][1] + b[1][1] * a[1][1];
+            b00 %= MODX, b01 %= MODX, b10 %= MODX, b11 %= MODX;
+            b[0][0] = b00;
+            b[0][1] = b01;
+            b[1][0] = b10;
+            b[1][1] = b11;
+        }
+    }
+    fout << b[0][1] << '\n';
 }
